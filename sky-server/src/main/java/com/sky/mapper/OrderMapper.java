67ae87db0ec5c32b8a5e40ff73ee0fae08c52afd
lo <lohuyendieu1@gmail.com>
2026-04-07 17:05:43 +0800

    feat:訂單支付功能代碼開發

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -2,6 +2,10 @@ package com.sky.mapper;
 
 import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import java.time.LocalDateTime;
 
 @Mapper
 public interface OrderMapper {
@@ -11,4 +15,21 @@ public interface OrderMapper {
      * @param order
      */
     void insert(Orders order);
+
+    /**
+     * 根據訂單號和用戶id查詢訂單
+     * @param orderNumber
+     * @param userId
+     */
+    @Select("select * from orders where number = #{orderNumber} and user_id= #{userId}")
+    Orders getByNumberAndUserId(String orderNumber, Long userId);
+
+    /**
+     * 修改訂單信息
+     * @param orders
+     */
+    void update(Orders orders);
+
+    @Update("update orders set status = #{orderStatus},pay_status = #{orderPaidStatus} ,checkout_time = #{check_out_time} where id = #{id}")
+    void updateStatus(Integer orderStatus, Integer orderPaidStatus, LocalDateTime check_out_time, Long id);
 }
--- sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
@@ -1,25 +1,25 @@
 package com.sky.service.impl;
 
+import com.alibaba.fastjson.JSONObject;
 import com.sky.constant.MessageConstant;
 import com.sky.context.BaseContext;
+import com.sky.dto.OrdersPaymentDTO;
 import com.sky.dto.OrdersSubmitDTO;
-import com.sky.entity.AddressBook;
-import com.sky.entity.OrderDetail;
-import com.sky.entity.Orders;
-import com.sky.entity.ShoppingCart;
+import com.sky.entity.*;
 import com.sky.exception.AddressBookBusinessException;
+import com.sky.exception.OrderBusinessException;
 import com.sky.exception.ShoppingCartBusinessException;
-import com.sky.mapper.AddressBookMapper;
-import com.sky.mapper.OrderDetailMapper;
-import com.sky.mapper.OrderMapper;
-import com.sky.mapper.ShoppingCartMapper;
+import com.sky.mapper.*;
 import com.sky.service.OrderService;
+import com.sky.utils.WeChatPayUtil;
+import com.sky.vo.OrderPaymentVO;
 import com.sky.vo.OrderSubmitVO;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -38,6 +38,9 @@ public class OrderServiceImpl implements OrderService {
 
     @Autowired
     private ShoppingCartMapper  shoppingCartMapper;
+
+    // 這裡注入一個 Orders 對象，方便後續獲取當前用戶的訂單id，實際開發中不建議這麼做，這裡是為了跳過支付環節方便測試
+    private Orders orders;
     /**
      * 用戶下單
      * @param ordersSubmitDTO
@@ -67,16 +70,17 @@ public class OrderServiceImpl implements OrderService {
 
         // 向訂單表插入 1 條數據
 
-        Orders order = new Orders();
-        BeanUtils.copyProperties(ordersSubmitDTO,order);
-        order.setOrderTime(LocalDateTime.now());
-        order.setPayStatus(Orders.UN_PAID);
-        order.setStatus(Orders.PENDING_PAYMENT);
-        order.setNumber(String.valueOf(System.currentTimeMillis())); // 生成一個唯一的訂單號，這裡簡單的用當前時間戳來生成
-        order.setPhone(addressBook.getPhone());
-        order.setConsignee(addressBook.getConsignee());
-        order.setUserId(userId);
-        orderMapper.insert(order);
+        Orders orders = new Orders();
+        BeanUtils.copyProperties(ordersSubmitDTO,orders);
+        orders.setOrderTime(LocalDateTime.now());
+        orders.setPayStatus(Orders.UN_PAID);
+        orders.setStatus(Orders.PENDING_PAYMENT);
+        orders.setNumber(String.valueOf(System.currentTimeMillis())); // 生成一個唯一的訂單號，這裡簡單的用當前時間戳來生成
+        orders.setPhone(addressBook.getPhone());
+        orders.setConsignee(addressBook.getConsignee());
+        orders.setUserId(userId);
+        this.orders = orders; // 跳過支付加上這句
+        orderMapper.insert(orders);
 
         List<OrderDetail> orderDetailList = new ArrayList<>(); // 方便後續批量插入訂單明細數據
 
@@ -84,7 +88,7 @@ public class OrderServiceImpl implements OrderService {
         for (ShoppingCart cart : shoppingCartList) {
             OrderDetail orderDetail = new OrderDetail(); // 訂單明細
             BeanUtils.copyProperties(cart,orderDetail);
-            orderDetail.setOrderId(order.getId()); // 設置當前訂單明細對應的訂單id
+            orderDetail.setOrderId(orders.getId()); // 設置當前訂單明細對應的訂單id
             orderDetailList.add(orderDetail);
         }
 
@@ -96,14 +100,73 @@ public class OrderServiceImpl implements OrderService {
         // 封裝 vo 返回結果
 
         OrderSubmitVO orderSubmitVO = OrderSubmitVO.builder()
-                .id(order.getId())
-                .orderTime(order.getOrderTime())
-                .orderNumber(order.getNumber())
-                .orderAmount(order.getAmount())
+                .id(orders.getId())
+                .orderTime(orders.getOrderTime())
+                .orderNumber(orders.getNumber())
+                .orderAmount(orders.getAmount())
                 .build();
 
         return orderSubmitVO;
     }
 
+    @Autowired
+    private UserMapper userMapper;
+    @Autowired
+    private WeChatPayUtil weChatPayUtil;
+
+    /**
+     * 订单支付
+     * @param ordersPaymentDTO
+     * @return
+     */
+    public OrderPaymentVO payment(OrdersPaymentDTO ordersPaymentDTO) throws Exception {
+        // 当前登录用户id
+        Long userId = BaseContext.getCurrentId();
+        User user = userMapper.getById(userId);
+/*        // 调用微信支付接口，生成预支付交易单
+        JSONObject jsonObject = weChatPayUtil.pay(
+                ordersPaymentDTO.getOrderNumber(), // 商户订单号
+                new BigDecimal(0.01), // 支付金额，单位 元
+                "苍穹外卖订单", // 商品描述
+                user.getOpenid() // 微信用户的openid
+        );
+
+        if (jsonObject.getString("code") != null && jsonObject.getString("code").equals("ORDERPAID")) {
+            throw new OrderBusinessException("该订单已支付");
+        }
+*/
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("code","ORDERPAID");
+        OrderPaymentVO vo = jsonObject.toJavaObject(OrderPaymentVO.class);
+        vo.setPackageStr(jsonObject.getString("package"));
+        Integer OrderPaidStatus = Orders.PAID; // 支付状态，已支付
+        Integer OrderStatus = Orders.TO_BE_CONFIRMED;  // 订单状态，待接单
+        LocalDateTime check_out_time = LocalDateTime.now();// 更新支付时间
+        orderMapper.updateStatus(OrderStatus, OrderPaidStatus, check_out_time, this.orders.getId());
+        return vo;
+    }
+
+    /**
+     * 支付成功，修改訂單狀態
+     *
+     * @param outTradeNo
+     */
+    public void paySuccess(String outTradeNo) {
+        // 當前登錄用戶id
+        Long userId = BaseContext.getCurrentId();
+
+        // 根據訂單號查詢當前用戶的訂單
+        Orders ordersDB = orderMapper.getByNumberAndUserId(outTradeNo, userId);
+
+        // 根據訂單id更新訂單的狀態、支付方式、支付狀態、結帳時間
+        Orders orders = Orders.builder()
+                .id(ordersDB.getId())
+                .status(Orders.TO_BE_CONFIRMED)
+                .payStatus(Orders.PAID)
+                .checkoutTime(LocalDateTime.now())
+                .build();
+
+        orderMapper.update(orders);
+    }
 
 }
